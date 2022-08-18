3f0d2b4847f53932485784369a7b5b0dc2aa7f88
Maicon Leandro <snubhousedev@gmail.com>
2022-08-17 23:33:53 -0300

    Personalizando o modo como os modelos são exibidos na área Admin

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Opportunity
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Opportunity)
+class OpportunityAdmin(admin.ModelAdmin):
+    list_display = ['category', 'title', 'slug', 'author',
+                    'image', 'wage', 'description', 'activated',
+                    'create', 'update', 'status']
+    list_display = ['category', 'title', 'activated']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'activated'
+    ordering = ['status', 'activated']
+
